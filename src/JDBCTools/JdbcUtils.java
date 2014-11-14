81b118437b9a4e16ae4a14b75010eb9bb0b2e0ca
gaoyangyiqiao <gyyq1994@gmail.com>
2014-11-15 00:36:32 +0800

    完善了数据库的增删查改，将连接数据库所需要的类添加到了类库中

--- src/JDBCTools/JdbcUtils.java
+++ src/JDBCTools/JdbcUtils.java
@@ -12,7 +12,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
+import PO.CustomerPO;
+import ResultMessage.ResultMessage;
 
 public class JdbcUtils {
 	// 定义数据库的用户名
@@ -34,7 +35,7 @@ public class JdbcUtils {
 	public JdbcUtils() {
 		try {
 			Class.forName(DRIVER);
-//			System.out.println("1234567");//test
+			// System.out.println("1234567");//test
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -213,7 +214,6 @@ public class JdbcUtils {
 			try {
 				resultSet.close();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -221,7 +221,6 @@ public class JdbcUtils {
 			try {
 				pstmt.close();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -229,45 +228,155 @@ public class JdbcUtils {
 			try {
 				connection.close();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	//模糊查找
+	//maps=jdbcUtils.findPO("CustomerPO","name","k");//maps为所有的name中包含k的PO的集合
+	public List<Map<String,Object>> fuzzySerch(String nameOfPO, String key,
+			String value){
+		String sql = "select * from " + nameOfPO + " where " + key + " like "
+				+ "'%" + value + "%'";
+		
+		List<Map<String,Object>> maps=new ArrayList<Map<String,Object>>();
+		try {
+			maps=this.findMoreResult(sql, null);
+			return maps;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			//返回空值则查找失败
+			return null;
+		}					
+	}
+
+	//查找操作
+	// 通过PO的属性和属性名获得其他属性
+	// 范例：
+	// Map<String,Object>
+	// myMap=jdbcUtils.findPO("CustomerPO","id","1");//查找id为1的customer，返回其所有属性
+	// System.out.println(myMap);
+	public Map<String, Object> findPO(String nameOfPO, String key,
+			String value) {
+		Map<String, Object> map;
+		String sql = "select * from " + nameOfPO + " where " + key + " = "
+				+ "'" + value + "'";
+
+		try {
+			map = this.findSimpleResult(sql, null);
+			return map;
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	//增加操作
+	// 增加对象到数据库,建议大家根据自己的类写出类似的类
+	public ResultMessage addPO(CustomerPO po) {
+		//在这里写出对象的所有属性以及后面的占位符
+		String sql = "insert into CustomerPO(number,sort,level,name,phone,address,zipcode,mail,clerk) values(?,?,?,?,?,?,?,?,?)";
+		List<Object> params=new ArrayList<Object>();
+		//按照上面的顺序填写po的属性
+		params.add(po.getNumber());
+		params.add(po.getSort());
+		params.add(po.getLevel());
+		params.add(po.getName());
+		params.add(po.getPhone());
+		params.add(po.getAddress());
+		params.add(po.getZipCode());
+		params.add(po.getMail());
+		params.add(po.getClerk());
+		try {
+			//
+			boolean flag=this.updateByPreparedStatement(sql,params);
+			return ResultMessage.add_success;
+			//需要返回一些提示成功
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			return ResultMessage.add_failure;
+		}
+		
+	}
+	
+	//删除操作
+	public ResultMessage deletePO(String nameOfPO, String key,
+			String value){
+		//删除key为value的一行
+		String sql="delete from "+nameOfPO+" where "+key+" = '"+value+"'";
+		try {
+			this.updateByPreparedStatement(sql,null);
+			return ResultMessage.delete_success;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return ResultMessage.delete_success;
+
+		}
+		
+	}
+	
+	//修改操作
+	public ResultMessage updatePO(String nameOfPO, String keyForFind,String valueForFind,String keyToChange,
+			String valueToChange){
+		//将nameofpo数据库中满足keyToFind=valueToFind的一行的keyTochange修改为valueToChange		
+		String sql="update "+nameOfPO+" set "+keyToChange+" = "+valueToChange+" where "+keyForFind+" = "+"'"+valueForFind+"'"; 
+		try {
+			this.updateByPreparedStatement(sql,null);
+			return ResultMessage.update_success;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResultMessage.update_failure;
+		}
+		
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		//使用前需要获得与数据库的连接，使用后需要释放连接
 		// TODO Auto-generated method stub
 		JdbcUtils jdbcUtils = new JdbcUtils();
-		//获取与数据库的连接
+		// 获取与数据库的连接
 		jdbcUtils.getConnection();
-		//sql语句，删除类似
-		 String sql = "insert into CustomerPO(number,sort,level,name) values(?,?,?,?)";
-		 List<Object> params = new ArrayList<Object>();
-		 params.add("0001");		 
-		 params.add("importer");
-		 params.add("firstClass");
-		 params.add("kmno4");
-		 try {
-		 boolean flag = jdbcUtils.updateByPreparedStatement(sql, params);
-		 System.out.println(flag);
-		 } catch (SQLException e) {
-		 e.printStackTrace();
-		 }
-//		String sql = "select * from userinfo ";
-//		// List<Object> params = new ArrayList<Object>();
-//		// params.add(1);
-//		try {
-//			List<Map<String, Object>> map = jdbcUtils.findMoreResult(sql, null);
-//			System.out.println(map);
-//		} catch (Exception e) {
-//			// TODO: handle exception
-//			e.printStackTrace();
-//		} finally {
-//			jdbcUtils.releaseConn();
-//		}
-	}
 
+		// 增加操作的范例
+		// String sql =
+		// "insert into CustomerPO(number,sort,level,name) values(?,?,?,?)";
+		// List<Object> params = new ArrayList<Object>();
+		// params.add("0001");
+		// params.add("importer");
+		// params.add("firstClass");
+		// params.add("kmno4");
+		// try {
+		// boolean flag = jdbcUtils.updateByPreparedStatement(sql, params);
+		// System.out.println(flag);
+		// } catch (SQLException e) {
+		// e.printStackTrace();
+		// }
+
+		// 查找操作的范例
+		// String sql = "select * from CustomerPO where id like '%1%'";
+		// // List<Object> params = new ArrayList<Object>();
+		// // params.add(1);
+		// try {
+		// List<Map<String, Object>> map = jdbcUtils.findMoreResult(sql, null);
+		// CustomerPO po=new CustomerPO();
+		// System.out.println(map.get(0).get("name"));
+		// // System.out.println(map);
+		// } catch (Exception e) {
+		// // TODO: handle exception
+		// e.printStackTrace();
+		// } finally {
+		// jdbcUtils.releaseConn();
+		// }
+
+		jdbcUtils.releaseConn();
+	}
 }
