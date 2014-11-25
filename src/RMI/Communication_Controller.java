a9a5aaa7bcc9fc6f3d09302effcb77cfc42a5612
shengxiaoyu <shengxiaoyu>
2014-11-25 17:51:04 +0800

    财务逻辑实现

--- src/RMI/Communication_Controller.java
+++ src/RMI/Communication_Controller.java
@@ -5,10 +5,12 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 
 import DataService.FinanceDataService.FinanceController;
+import DataService.InfoDataService.InfoController;
 import DataService.PromotionDataService.PromotionController;
 import DataService.UserDataService.UserController;
 import PO.AccountPO;
 import PO.PromotionPO;
+import PO.ScreeningConditionPO;
 import PO.UserPO;
 import ResultMessage.ResultMessage;
 
@@ -79,6 +81,31 @@ public class Communication_Controller extends UnicastRemoteObject implements Com
 	public ArrayList<Object> showObject(String command)
 			throws RemoteException {
 		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public ArrayList<Object> showReceipt(String command,
+			ScreeningConditionPO condition) throws RemoteException {
+		// TODO Auto-generated method stub
+		if(command.equals("showSalesDetailsInfo")){
+			return new InfoController().findReceipt(condition) ;
+		}
+		if(command.equals("showSalesProcessInfo")){
+			return new InfoController().findReceipt(condition) ;
+		}
+		return null;
+	}
+
+
+	@Override
+	public String showSalesConditionInfo(String command, String time1, String time2)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		if(command.equals("showSalesConditionInfo")){
+			return new InfoController().getSalesCondition(time1, time2) ;
+		}
 		return null;
 	}
 
