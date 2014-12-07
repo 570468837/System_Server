9ebdd91d6d02774bc5e211acd03e9eab9580f87d
gaoyangyiqiao <gyyq1994@gmail.com>
2014-12-07 16:59:50 +0800

    vvvvvvvvv

---
+++ src/Config/Level.java
@@ -0,0 +1,9 @@
+package Config;
+
+/**
+ * @author gaoyang
+ *用来保存客户的等级
+ */
+public enum Level {
+	firstClass,secondClass,thirdClass,forthClass,fiveClassVIP;
+}
---
+++ src/Config/UserSort.java
@@ -0,0 +1,11 @@
+package Config;
+
+import java.io.Serializable;
+
+public enum UserSort{
+	Commodity,
+	PurchaseAndSaler,
+	Finance,
+	Manager,
+	Admin,
+}
