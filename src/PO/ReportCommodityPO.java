fb7503bcbbd08c7ebaee703fe4eea344a05fe516
Bagnogetoff <1051943988@qq.com>
2014-12-11 01:00:06 +0800

    写了两个界面

--- src/PO/ReportCommodityPO.java
+++ src/PO/ReportCommodityPO.java
@@ -12,9 +12,6 @@ public class ReportCommodityPO {
 	public int num;
 	public Date date;
 	
-	public ReportCommodityPO() {
-		
-	}
 	
 	
 	
--- src/PO/SendCommodityPO.java
+++ src/PO/SendCommodityPO.java
@@ -12,24 +12,10 @@ public class SendCommodityPO {
 	public long goodsPOId;
 	public String customerPOName;
 	public int num;
-	public int type = UNCHECKED;
+	public int checked;
 	public Date date;
 	
-	public SendCommodityPO() {}
-	/**
-	 * 赠送单的创建方法，并自动生成创建时间
-	 */
-	public SendCommodityPO(long goodsPOId, String customerPOName, int num) {
-		this.goodsPOId = goodsPOId;
-		this.customerPOName = customerPOName;
-		this.num = num;
-		date = new Date();//自动生成系统时间
-	}
-	public SendCommodityPO(SendCommodityPO po) {
-		this.goodsPOId = po.goodsPOId;
-		this.customerPOName = po.customerPOName;
-		this.num = po.num;
-	}
+	
 	
 	public static final int UNCHECKED = 0;
 	public static final int PASS = 1;
